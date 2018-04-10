1536340f7018fba4033806d40443bd08001415d3
zoltanpataki <zolee7777@gmail.com>
2018-04-10 09:51:31 +0200

    initial commit

---
+++ src/main/java/com/codecool/sportSite/Launch.java
@@ -0,0 +1,12 @@
+package com.codecool.sportSite;
+
+import com.codecool.sportSite.Service.UserService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Launch {
+    public static void main(String[] args) {
+        SpringApplication.run(UserService.class, args);
+    }
+}
---
+++ src/main/java/com/codecool/sportSite/Service/UserService.java
@@ -0,0 +1,85 @@
+package com.codecool.sportSite.Service;
+
+import com.codecool.sportSite.Model.User;
+import com.codecool.sportSite.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+import java.util.Map;
+
+@Service
+public class UserService {
+
+
+    @Autowired
+    UserRepository userRepository;
+
+    public void register(Map<String, String> requestParams){
+        String username = requestParams.get("username");
+        String password = requestParams.get("password");
+        String email = requestParams.get("email");
+        String firstname = requestParams.get("firstname");
+        String lastname = requestParams.get("lastname");
+        if (checkRegistrationDatas(username, password, email, firstname, lastname)){
+
+        }
+        try {
+            if(username.length() > 4 || password.length() > 4) {
+                User newUser = new User(firstname, lastname, username, password, email);
+                userRepository.save(newUser);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+
+    public boolean checkRegistrationDatas(String username, String password, String email, String firstname, String lastname){
+        return (username.length() < 4 || password.length() < 4 || email.length() < 4 || firstname.length() < 1 || lastname.length() < 2);
+    }
+
+    public void login(Map<String, String> requestParams, HttpSession session) {
+        String username = requestParams.get("username");
+        String password = requestParams.get("password");
+        try {
+            User result = userRepository.findByUsername(username);
+            String id = String.valueOf(result.getId());
+            if (result.getUsername().equals(username) && result.getPassword().equals(password)) {
+                session.setAttribute("userId", id);
+            }
+        } catch (javax.persistence.NoResultException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public boolean userExists(String username){
+        try {
+            User result = userRepository.findByUsername(username);
+            return false;
+        } catch (javax.persistence.NoResultException e){
+            System.out.println(e);
+            return true;
+        }
+    }
+
+    public void logoutUser(HttpSession session) {
+        session.removeAttribute("userId");
+    }
+
+    public int getUserId(HttpSession session) {
+        int userId;
+        try {
+            userId = Integer.parseInt(session.getAttribute("userId").toString());
+        } catch (NullPointerException | NumberFormatException e) {
+            userId = -1;
+        }
+        return userId;
+    }
+
+    public User getUserById(Long id){
+        return userRepository.findOne(id);
+
+    }
+}
